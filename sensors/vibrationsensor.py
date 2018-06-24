b09deb0fc7e5b25d98aa99348c521cd5f40ea873
yashg56 <34195669+yashg56@users.noreply.github.com>
2018-06-23 17:32:52 -0700

    6/23/2018 Final VBS Update

--- sensors/vibrationsensor.py
+++ sensors/vibrationsensor.py
@@ -1,20 +1,41 @@
 import RPi.GPIO as GPIO
 import time
+import smtplib
  
 #GPIO SETUP
 channel = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
- 
+callback_timestamp = time.time()
+email_address = "noreplyappmonitornotifications@gmail.com"
+
 def callback(channel):
-    if GPIO.input(channel):
-            print(GPIO.input(channel), end="")
-    else:
-            printf("To: ninadmore2@gmail.com\nFrom: RaspberryPi3\nSubject: Testing send mail from Raspberry\n\nThis is test. Best Regards!\n")
+    global callback_timestamp
+    callback_timestamp = time.time()    
+    
+        
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email_address, "corporatex")
+    msg = "This is a test email!"
+    server.sendmail(email_address, email_address, msg)
+    server.quit()
+    print("Sending mail...")
+    time.sleep(20)
+
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
  
 # infinite loop
 while True:
-        time.sleep(1)
+    current_time = time.time()
+    print(current_time)
+    print(callback_timestamp)
+    print("------------------------")
+
+    if((current_time - callback_timestamp) > 30):
+        send_mail()
+    
+    time.sleep(1)
